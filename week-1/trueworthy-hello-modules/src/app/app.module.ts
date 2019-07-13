ebbcc5743b0f3e5019d216c4c7ce1d55c50ed7de
Lea <ltrueworthy@yahoo.com>
2019-07-13 11:51:39 -0700

    shipping 1.5

--- week-1/trueworthy-hello-modules/src/app/app.module.ts
+++ week-1/trueworthy-hello-modules/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShippingModule } from "./shipping/shipping.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ShippingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ week-1/trueworthy-hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-shipping',
+    template: `
+    <p>
+      Created by Lea. 
+      
+    </p>
+    <p>
+    Here is where the magic will happen!
+    </p>
+  `,
+    styles: []
+})
+export class ShippingComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
